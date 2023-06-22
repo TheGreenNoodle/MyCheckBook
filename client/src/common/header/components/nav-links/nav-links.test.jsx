6ec966304e2e2cb75fa4e2bc305398d09f6cbc63
Anton Fleming <aflemingrocks089@gmail.com>
2023-06-21 23:56:21 -0500

    Wrote some unit test for nav-links.jsx
    Wrote a unit test to make sure the svg loads when no error is present and the pngs load an img onError. Working on a test to make sure the svgs and pngs exist in the project folder and are in the correct dir. Then I will work on adding a test to ensure that the nav-links take you to the right page url.

--- client/src/common/header/components/nav-links/nav-links.test.jsx
+++ client/src/common/header/components/nav-links/nav-links.test.jsx
@@ -1,26 +1,58 @@
-import { render, screen } from "@testing-library/react";
+// jest testing kit
+import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
+// img dir path data
+import { named, noName } from "./link-data";
+
+// components
 import NavLinks from "./nav-links";
 
-test("header links are directing to proper react-router endpoints", () => {
-  return false;
-});
-describe("imgs load correct file based on if an error is thrown", () => {
-  test("when error is not thrown load SVG", async () => {
-    render(<NavLinks />);
-    const imgs = await screen.findAllByRole("img");
+// file system explorer
+const fs = require("fs");
 
-    // make sure all btns have an img
-    expect(imgs.length).toBe(6);
+// test("header links are directing to proper react-router endpoints", () => {
+//   return false;
+// });
 
-    // makes sure all imgs are right img type
-    for (let i = 0; i < imgs.length; i++) {
-      console.log(imgs[i].src.endsWith(".svg"));
-      expect(imgs[i].src).endsWith(".svg");
-    }
+describe("icons", () => {
+  test("files exist in right dir path", () => {
+    const linkData = named.concat(noName);
+
+    expect(fs.existsSync("./link-data.js")).toBe(true);
+
+    // for (const element of linkData) {
+    //   // svgs exist in right path
+    //   expect(fs.existsSync(element.svg)).toBe(true);
+    // }
   });
-  test("when error is thrown load PNG", () => {
-    return false;
+
+  describe("imgs load correct file based on if an error is thrown", () => {
+    test("when error is not thrown load SVG", async () => {
+      render(<NavLinks />);
+      const imgs = await screen.findAllByRole("img");
+
+      // make sure all btns have an img
+      expect(imgs.length).toBe(6);
+
+      // make sure that it is the svgs that are loaded
+      for (let i = 0; i < imgs.length; i++) {
+        expect(imgs[i].src.endsWith(".svg")).toBe(true);
+      }
+    });
+
+    test("when error is thrown load PNG", async () => {
+      render(<NavLinks />);
+      const imgs = await screen.findAllByRole("img");
+
+      // make sure all btns have an img
+      expect(imgs.length).toBe(6);
+
+      // make sure that it is the svgs that are loaded
+      for (let i = 0; i < imgs.length; i++) {
+        fireEvent.error(imgs[i]);
+        expect(imgs[i].src.endsWith(".png")).toBe(true);
+      }
+    });
   });
 });
